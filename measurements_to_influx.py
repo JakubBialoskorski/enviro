2ca7f3427cce392b8d9c85204c239ace70660d09
Jakub Białoskórski <bialoskorski@icloud.com>
2023-06-04 10:20:51 +0200

    add PMS5003 configuration

--- measurements_to_influx.py
+++ measurements_to_influx.py
@@ -23,6 +23,14 @@ client = InfluxDBClient(host, port, user, password, dbname, ssl=True, verify_ssl
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
+# Configure the PMS5003 for Enviro+
+pms5003 = PMS5003(
+    device='/dev/ttyAMA0',
+    baudrate=9600,
+    pin_enable=22,
+    pin_reset=27
+)
+
 # Get the temperature of the CPU for compensation
 def get_cpu_temperature():
     with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
@@ -47,13 +55,17 @@ while True:
     oxidising = gas_data.oxidising
     reducing = gas_data.reducing
     nh3 = gas_data.nh3
-
     # oxidising, NO2: ppm = Rs /(6.5 * R0)
     NO2_PPM = gas_data.oxidising/6.5/1230000 # R0 chosen to give value approx 0.01 in fresh air (detectable = 0.05 to 10)
     # reducing,CO: ppm = 10^((log10(Rs / 3.5 / R0)) / -0.845 )
     CO_PPM = 10**((log10(gas_data.reducing/3.5/162000)) / -0.845) # R0 chosen to give value approx 2 in fresh air (detectable = 1 to 1000)
     # NH3: ppm = =10^((log10(Rs / 0.77 / R0)) / -0.5335 )
     NH3_PPM = 10**((log10(gas_data.nh3/0.77/298000)) / -0.5335) # R0 chosen to give value approx 2 in fresh air (detectable = 1 to 300)
+    # PMS5003 readings
+    data = pms5003.read()
+    pms5003_pm1 = data.pm_ug_per_m3(1.0)
+    pms5003_pm25 = data.pm_ug_per_m3(2.5)
+    pms5003_pm10 = data.pm_ug_per_m3(10)
 
     # Hack time to properly display in Grafana
     iso = time.strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
@@ -73,7 +85,10 @@ while True:
                 "nh3": gas_data.nh3,
                 "oxidising_ppm": NO2_PPM,
                 "reducing_ppm": CO_PPM,
-                "nh3_ppm": NH3_PPM
+                "nh3_ppm": NH3_PPM,
+                "pm1": pms5003_pm1,
+                "pm25": pms5003_pm25,
+                "pm10": pms5003_pm10
             }
         }
     ]
